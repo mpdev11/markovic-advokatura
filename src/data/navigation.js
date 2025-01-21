f2b97757531768f3def2d6974cdab4d4d7a8bb9e
mpdev11 <milospericdev@protonmail.com>
2025-01-21 14:30:07 +0100

    Added footer component; Added navigation file;

---
+++ src/data/navigation.js
@@ -0,0 +1,6 @@
+export const navigation = [
+  { label: "O meni", path: "/o-meni" },
+  { label: "Advokatske usluge", path: "/advokatske-usluge" },
+  { label: "Advokatska tarifa", path: "/advokatska-tarifa" },
+  { label: "Kontakt", path: "/kontakt" },
+];
