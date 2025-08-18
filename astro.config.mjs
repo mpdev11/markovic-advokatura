8d62c8153f7dd960ded2268aa7f686fb955b9adf
mpdev11 <milospericdev@protonmail.com>
2025-08-19 00:27:54 +0200

    Astro config tweaks.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import alpinejs from '@astrojs/alpinejs';
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  integrations: [tailwind(), alpinejs()],
+  site: "https://mpdev11.github.io", // optional, good for sitemaps, RSS, etc.
+  base: "/markovic-advokatura/",
 });
